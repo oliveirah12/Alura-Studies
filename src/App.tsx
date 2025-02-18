636183cff78f321f4f54df12a2ce1c4b8b7198be
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-02-18 17:31:49 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Formulario from './components/Formulario/index'
+import Lista from './components/Lista';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Formulario />
+        <Lista />
+      </header>
+    </div>
+  );
+}
+
+export default App;
