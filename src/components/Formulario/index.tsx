56eeaec41a06211907894bac5bc4d6ba2ffa3ff3
Matheus Oliveira <matheus-homem2001@hotmail.com>
2025-02-19 22:28:58 -0300

    Começando - Adicionando uma tarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -10,16 +10,22 @@ class Formulario extends React.Component{
         tarefa: "",
         tempo: "00:00"
     }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault()
+    }
     
     render(){
         return(
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo estudo</label>
                     <input 
                         type="text" 
                         name="tarefa" 
                         id="tarefa" 
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                         placeholder="O que estudar?" 
                         required/>
                 </div>
