ffe93c1ee9cc162bd163d2fbf80e2ccaf0f42a0f
Matheus Oliveira <matheus-homem2001@hotmail.com>
2025-02-22 15:15:00 -0300

    Terminado Tarefa selecionada

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,10 +1,21 @@
 import { ITarefa } from '../../../types/Tarefa'
 import style from './../Lista.module.scss'
 
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa){
+export default function Item({tarefa, tempo, selecionado, completado, id, selecionaTarefa}: Props){
     return(
-        <li className={style.item}>
+        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+            onClick={() => selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id
+            })}>
+
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>
